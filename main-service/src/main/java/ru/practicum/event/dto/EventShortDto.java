eba123363fcd1f8293327d3402949cdc305bd1fc
Mao2201 <tsybin.a.a@lan-profi.ru>
2023-07-11 22:41:05 +0300

    исправил кодстайл

--- main-service/src/main/java/ru/practicum/event/dto/EventShortDto.java
+++ main-service/src/main/java/ru/practicum/event/dto/EventShortDto.java
@@ -2,7 +2,6 @@ package ru.practicum.event.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;

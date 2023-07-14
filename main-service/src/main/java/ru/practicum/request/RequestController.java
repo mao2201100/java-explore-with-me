a4e3241550e6f9ac667edec68657fc2b74271d72
Mao2201 <tsybin.a.a@lan-profi.ru>
2023-07-14 17:47:18 +0300

    1

--- main-service/src/main/java/ru/practicum/request/RequestController.java
+++ main-service/src/main/java/ru/practicum/request/RequestController.java
@@ -1,4 +1,4 @@
-package ru.practicum.request.controller;
+package ru.practicum.request;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
--- main-service/src/main/java/ru/practicum/user/UserController.java
+++ main-service/src/main/java/ru/practicum/user/UserController.java
@@ -1,4 +1,4 @@
-package ru.practicum.user.controller;
+package ru.practicum.user;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;

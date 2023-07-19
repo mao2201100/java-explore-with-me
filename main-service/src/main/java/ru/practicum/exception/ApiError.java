063a7e1fd9e1428658753a97164b3fc649a23aa7
Mao2201 <tsybin.a.a@lan-profi.ru>
2023-07-19 20:49:21 +0300

    Поправил постман тесты

--- main-service/src/main/java/ru/practicum/exception/ApiError.java
+++ main-service/src/main/java/ru/practicum/exception/ApiError.java
@@ -2,12 +2,15 @@ package ru.practicum.exception;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 @Builder
 public class ApiError {
 
